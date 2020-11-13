d61ae6b6ffa60fc7ba33a4d19cc910e765a61b3d
Olivier <olivier.charlier.web@gmail.com>
2020-11-13 15:48:34 +0100

    ex 11.4

--- 11-fetch/03-details/script.js
+++ 11-fetch/03-details/script.js
@@ -10,5 +10,42 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    
+   
+
+    document.querySelector("#run").addEventListener("click", () => {
+        let input = +(document.getElementById("hero-id").value);
+            
+        // Récupérer les infos du serveur local comme pour l'ex 11.1
+        fetch("http://localhost:3000/heroes")
+          .then(function (dataHeroes) {
+            return dataHeroes.json();
+          })
+          .then(function (dataHeroes) {
+            dataHeroes.forEach(function (hero)  {
+            let template = document.getElementById("tpl-hero").content;
+            // Récupérer contenu tpl-hero a cloner dans nouvel emplacement
+            let location = document.getElementById("target");
+                    
+                let templateClone = template.cloneNode(true); //cloner template
+
+                if (input === hero.id) {
+              templateClone.querySelector(".name").innerHTML = hero.name; //mettre tout au bon endroit pour chaque héro
+              templateClone.querySelector(".alter-ego").innerHTML = hero.alterEgo;
+              templateClone.querySelector(".powers").innerHTML = hero.abilities.join(", "); // on JOIN pour que les abilities soient listés avec des virgules.
+    
+              location.appendChild(templateClone); //le nouveau template cloné dans nouvel emplacement
+              }
+            });
+        
+
+          //  document.getElementById("tpl-hero").remove(); //je supprime ancien emplacement pour code plus propre
+          });
+      });
+
+
+
+
+
+
 })();
